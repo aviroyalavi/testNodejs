9f6dcd645ae0b878f5ca0df64f1345aaac579fb7
aviroyalavi <aboutrajahmundry@gmail.com>
2020-09-18 18:59:21 +0530

    make it better

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 var http = require('http');
-var fs = require('fs');
 
 http.createServer(function(req, res){
-	var fileName = "fil.html";
-	fs.appendFile(fileName, '\n3rd line', function(err){		
-		if(err) throw err;
-		res.write("Appended");
+	
+	
+		
+		res.write("Welcome");
 		res.end();
-	});
+	
 }).listen(8080);
